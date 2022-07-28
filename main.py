fa3deea319153fa1c969b6330b4ee44ad1a200df
Umair <muhammadumair2511@gmail.com>
2022-07-28 13:18:37 +0500

    completed the folder creation function, move file function remaining

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
+import shutil
 
 extensions = {
-    "xlsx" : "Spreadsheets",
-    "exe" : "Setup Files",
-    "png" : "Images",
-    "jpeg" : "Images",
-    "jpg" : "Images",
-    "svg" : "Images",
+    ".xlsx" : "Spreadsheets",
+    ".exe" : "Setup Files",
+    ".png" : "Images",
+    ".jpeg" : "Images",
+    ".jpg" : "Images",
+    ".svg" : "Images",
+    ".txt" : "Text",
+    ".zip" : "Zip"
 }
 
 downlaodFolderPath = 'C:/Users/Muhammad Umair Khan/Downloads'
@@ -16,17 +19,25 @@ def checkFiles():
     return len(availableFiles), availableFiles
     
 def getFileName(dir):
-    folder = []
+    exts = []
     for files in dir:
-        fileExt = files.split(".")  
-        folder.append(fileExt)      
-    return folder
-        
+        ext = os.path.splitext(files)
+        exts.append(ext[1])
+    createFolders(exts)
             
-def createFolders(ext):
-    folder = set(ext)
-    print(folder) 
+def createFolders(exts):
+    folder = set(exts)
+    for x in folder:
+        if x not in extensions.keys():
+            fpath = os.path.join(downlaodFolderPath, "Misc")
+        else:
+            fpath = os.path.join(downlaodFolderPath, extensions[x])
+
+        if not os.path.exists(fpath):
+            os.mkdir(fpath)
+
+
 
 dir = checkFiles()
-ext = getFileName(dir[1])
-print(ext)
+
+getFileName(dir[1])
